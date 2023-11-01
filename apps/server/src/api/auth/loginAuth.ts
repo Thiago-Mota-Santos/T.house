982242ca4615c203cef2bccc856a47789c0ffc56
Thiago <lucimara1422@gmail.com>
2023-11-01 14:56:08 -0300

    feat(auth) api add

---
+++ apps/server/src/api/auth/loginAuth.ts
@@ -0,0 +1,61 @@
+import type { ParameterizedContext } from "koa";
+import { generateToken } from "../../auth/generateToken";
+import UserModel from "../../modules/user/UserModel";
+
+interface UserBody {
+  email: string;
+  password: string;
+}
+
+export const loginAuth = async (ctx: ParameterizedContext) => {
+  const { email, password } = ctx.request.body as UserBody;
+
+  if (!email || !password) {
+    ctx.status = 401;
+    ctx.body = {
+      message: "email or password incorrect",
+    };
+
+    return;
+  }
+
+  const user = await UserModel.findOne({
+    email,
+    removedAt: null,
+  });
+
+  if (!user) {
+    ctx.status = 401;
+    ctx.body = {
+      message: "Email or password incorrect",
+    };
+  }
+
+  let correctPassword;
+
+  try {
+    correctPassword = user?.authenticate(password);
+  } catch {
+    ctx.status = 401;
+    ctx.body = {
+      message: "Email or password incorrect",
+    };
+
+    return;
+  }
+
+  if (!correctPassword) {
+    ctx.status = 401;
+    ctx.body = {
+      message: "Email or password incorrect",
+    };
+
+    return;
+  }
+
+  ctx.status = 200;
+  ctx.body = {
+    message: "User authenticated sucessfully",
+    token: generateToken(user),
+  };
+};
