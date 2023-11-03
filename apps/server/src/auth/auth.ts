1f497f0f9239e92bd060af6dc6fec70ad08c102d
Thiago <lucimara1422@gmail.com>
2023-11-03 19:10:10 -0300

    fix(auth) remove base 64

--- apps/server/src/auth/auth.ts
+++ apps/server/src/auth/auth.ts
@@ -3,48 +3,11 @@ import jwt from "jsonwebtoken";
 import type { User } from "../modules/user/UserModel";
 import { UserModel } from "../modules/user/UserModel";
 import { config } from "../config";
-import type { Base64String } from "./base64";
-import { unbase64 } from "./base64";
-
-export interface Token {
-  userId: string | null;
-}
-
-export const getToken = (authorization: Base64String): Token => {
-  const userId = unbase64(authorization);
-
-  if (!userId) {
-    return {
-      userId: null,
-    };
-  }
-
-  return {
-    userId,
-  };
-};
-
-export const getUser = async (token: string) => {
-  const { userId } = getToken(token);
-  if (!userId) {
-    return null;
-  }
-
-  const user = await UserModel.findOne({
-    _id: userId,
-    removedAt: null,
-  });
-
-  return user;
-};
 
 export const auth = async (ctx: ParameterizedContext, next: Next) => {
-  const { authorization } = ctx.header;
-  console.log(ctx.headers);
-  console.log(authorization);
+  const token = ctx.cookies.get("authToken");
 
-  // console.log(authorization);
-  if (!authorization) {
+  if (!token) {
     ctx.status = 401;
     ctx.body = {
       message: "Unauthorized",
@@ -52,22 +15,32 @@ export const auth = async (ctx: ParameterizedContext, next: Next) => {
     return;
   }
 
-  const user = await getUser(authorization);
+  try {
+    const subToken = token.replace("JWT ", "");
+    const decodedToken = jwt.verify(subToken, config.JWT_SECRET);
+    const decodedId = decodedToken as unknown as { user: string };
 
-  if (!user) {
-    ctx.status = 401;
+    const user = await UserModel.findOne({ _id: decodedId.user });
+    console.log(user);
+    if (!user) {
+      ctx.status = 401;
+      ctx.body = {
+        message: "Unauthorized",
+      };
+      return;
+    }
+
+    ctx.user = user;
+
+    await next();
+  } catch (e) {
+    ctx.status = 500;
     ctx.body = {
-      message: "Unauthorized",
+      message: "Some error occurred",
     };
-    return;
   }
-
-  ctx.user = user;
-
-  await next();
 };
 
 export const generateToken = (user: User) => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   return `JWT ${jwt.sign({ user: user._id }, config.JWT_SECRET)}`;
 };
