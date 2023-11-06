6b103a2bf521d0a04f20f84e9bbee007027111a8
Thiago <lucimara1422@gmail.com>
2023-11-06 14:04:52 -0300

    feat(getObjectId) global id using relay (yes, i'm crazy)

--- apps/server/src/test/getObjectId.ts
+++ apps/server/src/test/getObjectId.ts
@@ -1,8 +1,34 @@
-import { Types } from "mongoose";
-import type { Document } from "mongoose";
+import { fromGlobalId } from "graphql-relay";
+import { Types, Document } from "mongoose";
 
-export const getObjectId = (target: string | Document | Types.ObjectId) => {
+export const getObjectId = (
+  target: string | Document | Types.ObjectId,
+): Types.ObjectId | null => {
   if (target instanceof Types.ObjectId) {
     return new Types.ObjectId(target.toString());
   }
+
+  if (typeof target === "object" && target instanceof Document) {
+    return target?._id ? new Types.ObjectId(target._id) : null;
+  }
+
+  if (Types.ObjectId.isValid(target)) {
+    return new Types.ObjectId(target.toString());
+  }
+
+  if (typeof target === "string") {
+    const result = fromGlobalId(target);
+
+    if (result.type && result.id && Types.ObjectId.isValid(result.id)) {
+      return new Types.ObjectId(result.id);
+    }
+
+    if (Types.ObjectId.isValid(target)) {
+      return new Types.ObjectId(target);
+    }
+
+    return null;
+  }
+
+  return null;
 };
