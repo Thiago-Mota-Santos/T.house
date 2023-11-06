687a000b903b47733178c508f19e58f69b8d000d
Thiago <lucimara1422@gmail.com>
2023-11-06 11:40:34 -0300

    feat(app.ts) entry point

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,21 +1,40 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import logger from "koa-logger";
-import cors from "kcors";
 import Router from "koa-router";
+import cors from "kcors";
+import { userCreate } from "./api/userCreate";
 import { userGetAll } from "./api/userGetAll";
+import { auth } from "./auth/auth";
+import { loginAuth } from "./api/auth/loginAuth";
 
 const app = new Koa();
-const router = new Router();
 
-app.use(bodyParser());
+const routerAuth = new Router();
+const routerOpen = new Router();
 
 app.use(logger());
-app.use(cors({ credentials: true }));
+app.use(cors({ maxAge: 86400 }));
+app.use(bodyParser());
 
-router.get("/api/user", userGetAll);
+routerOpen.get("/api/version", (ctx) => {
+  ctx.status = 200;
+  ctx.body = {
+    status: "OK",
+  };
+});
 
-// Default not found 404
+routerOpen.post("/api/auth/login", loginAuth);
+
+app.use(routerOpen.routes());
+
+routerAuth.use(auth);
+
+routerAuth.post("/api/user", userCreate);
+routerAuth.get("/api/user", userGetAll);
+
+app.use(routerAuth.routes());
+
 app.use((ctx) => {
   ctx.status = 404;
 });
