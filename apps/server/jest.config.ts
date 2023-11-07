350168220b83ec35756b3ecf7b9dcd81ca3f8985
Thiago <lucimara1422@gmail.com>
2023-11-07 11:10:48 -0300

    feat(jest) esbuild to config

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -1,7 +1,14 @@
-// eslint-disable-next-line tsdoc/syntax
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+// import { } from "./src/test/environment/mongodb.ts"
+export default {
   moduleFileExtensions: ["ts", "js", "tsx", "json"],
+
+  preset: "ts-jest",
+  resetModules: false,
+  setupFiles: ["./src/test/jest.setup.ts"],
+  testEnvironment: "./src/test/environment/mongodb.ts",
+  testPathIgnorePatterns: ["/node_modules/"],
+
+  transform: {
+    "^.+\\.tsx?$": "esbuild-jest",
+  },
 };
