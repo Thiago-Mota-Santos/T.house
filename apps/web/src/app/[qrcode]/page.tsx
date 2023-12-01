75d614e1b96a55f3dcdc4dcd28fe311648eb089e
Thiago <lucimara1422@gmail.com>
2023-12-01 10:43:50 -0300

    feat(qrcode) component mounted

--- apps/web/src/app/[qrcode]/page.tsx
+++ apps/web/src/app/[qrcode]/page.tsx
@@ -1,23 +1,32 @@
+"use client";
 import { Box, Button, Card, Text } from "@radix-ui/themes";
 import { FileTextIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default function qrCode() {
+export default function QrCode() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/dashboard");
+  };
+
   const value = "R$ 16.50";
-  const desc = "long description about the product";
-
+  const desc =
+    "long description about the product description about the product";
   return (
     <div className="flex items-center justify-center h-screen">
-      <Box className="flex items-center justify-center flex-col w-[484px] h-[660px] border border-gray-400 rounded-3xl">
-        <Text align="center" size="6" weight="bold">
-          QRCODE
-        </Text>
-
-        <Box className="mt-2">
+      <Box className="flex items-center justify-center flex-col max-w-2xl w-[440px] h-[660px] border border-gray-400 rounded-3xl">
+        <Image alt="wallet icon" height="64" src="wallet.svg" width="64" />
+        <Box className="mt-2 max-w-sm">
           <div className="flex justify-between">
-            <Text>VALOR TOTAL</Text> <Text as="span">{value}</Text>
+            <Text weight="bold">VALOR TOTAL</Text>{" "}
+            <Text as="span">{value}</Text>
           </div>
-          <Text>DESCRIÇÃO: </Text> <Text as="span">{desc}</Text>
+          <Text weight="bold">DESCRIÇÃO </Text>
+          <Text as="span" className="block max-w-lg truncate">
+            {desc}
+          </Text>
         </Box>
 
         <Card mt="4" size="4">
@@ -28,10 +37,21 @@ export default function qrCode() {
             width={300}
           />
         </Card>
-        <Button color="purple" mt="5" radius="large">
-          Adicione outro QRCODE
+        <Button
+          className="hover:cursor-pointer"
+          color="purple"
+          mt="5"
+          onClick={handleRedirect}
+          radius="large"
+        >
+          Crie outro QRCODE
         </Button>
-        <Button color="gray" mt="5" radius="large">
+        <Button
+          className="hover:cursor-pointer"
+          color="gray"
+          mt="5"
+          radius="large"
+        >
           <FileTextIcon /> Imprimir
         </Button>
       </Box>
